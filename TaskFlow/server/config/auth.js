9ade69c550823ae03bef2f22b6349b0c5ef8a20e
Ulug2 <khaiul01@gettysburg.edu>
2025-11-18 21:03:24 -0500

    Update production URLs for frontend and OAuth callback

--- TaskFlow/server/config/auth.js
+++ TaskFlow/server/config/auth.js
@@ -2,10 +2,10 @@ import { Strategy as GitHub } from 'passport-github2'
 import { pool } from './database.js'
 
 const options = {
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/github/callback'
-}
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: 'https://taskflow-server-qgp4.onrender.com/auth/github/callback',
+};
 
 const verify = async (accessToken, refreshToken, profile, callback)  => {
     const { _json: { id, name, login, avatar_url } } = profile
